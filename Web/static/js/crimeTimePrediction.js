b0c6dbdad426dc014057182116897dafab7b9f38
Edgar Mercado <improveyourtomorrow@Edgars-iMac.local>
2020-06-24 20:18:41 -0700

    Final Commit

---
+++ Web/static/js/crimeTimePrediction.js
@@ -0,0 +1,16 @@
+d3.select('#Monday-list').append("li").text("Predicted Crime:")
+  .append("li").text("Time of Crime:")
+
+
+
+d3.select('#Tuesday-list').append("li").text("Testing!")
+
+
+d3.select('#Wednesday-list').append("li").text("Testing!")
+
+
+d3.select('#Thursday-list').append("li").text("Testing!")
+
+
+d3.select('#Friday-list').append("li").text("Testing!")
+
